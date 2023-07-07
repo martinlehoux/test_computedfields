8169ac370ff2e05f08c796b599b5839c33683162
Martin Kagamino Lehoux <martin@lehoux.net>
2023-07-07 10:46:41 +0200

    fix:

--- test.py
+++ test.py
@@ -8,13 +8,13 @@ tags = [
     Tag.objects.create(name="tag2"),
 ]
 
-print("advert 1 setup")
 advert_1 = Advert.objects.create()
+print(f"{advert_1} setup")
 advert_1.tags.add(*tags)
 
-print("advert 2 setup")
 advert_2 = Advert.objects.create()
+print(f"{advert_2} setup")
 advert_2.tags.add(*tags)
 
-print("advert 1 update")
+print(f"{advert_1} update")
 advert_1.tags.set([tags[0]])
